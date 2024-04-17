e20cce2848269238ce100eaefa67680cfb3686a4
Matt Davis <matthewpeterdavis@gmail.com>
2024-04-17 15:28:52 +0100

    add note about amount raised

--- src/templates/Fundraisers.tsx
+++ src/templates/Fundraisers.tsx
@@ -16,6 +16,11 @@ function Fundraisers() {
           else friends of people we know personally. Please support and donate
           to as many as you can. <br />
           <br />
+          <p className="text-xs italic">
+            Please note, the amount raised here may not update for a few hours
+            after donations, although it will update on the GoFundMe page
+            directly
+          </p>
           <br />
         </div>
         <GfmsContainer />
