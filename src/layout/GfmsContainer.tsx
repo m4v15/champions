3a35530888e487d14267bbe1c122dba9632004ce
Matt Davis <matthewpeterdavis@gmail.com>
2024-04-23 11:57:16 +0100

    add types

--- src/layout/GfmsContainer.tsx
+++ src/layout/GfmsContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 
 import { Card } from './Card';
@@ -7,6 +7,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export function GfmsContainer() {
   const { data, error, isLoading } = useSWR('/api/gfms', fetcher);
+  const [query, setQuery] = useState('');
 
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
@@ -42,27 +43,55 @@ export function GfmsContainer() {
       ) => a.rawProgress - b.rawProgress,
     );
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+  };
+
+  const multipleSearch = (array: []) => {
+    return array.filter((el: any) =>
+      Object.keys(el).some((parameter: string) =>
+        el[parameter].toString().toLowerCase().includes(query),
+      ),
+    );
+  };
+
+  const filtered = multipleSearch(formattedGfms);
+
   return (
-    <div className="flex flex-wrap">
-      {formattedGfms.map(
-        (gfm: {
-          title: string;
-          url: string;
-          imageurl: string;
-          progress: string;
-        }) => {
-          return (
-            <Card
-              title={gfm.title}
-              imageUrl={gfm.imageurl}
-              progress={gfm.progress}
-              url={gfm.url}
-              target=""
-              key={gfm.url}
-            />
-          );
-        },
-      )}
-    </div>
+    <>
+      <div className="ml-2 flex max-w-xs items-center">
+        <label htmlFor="filter" className="sr-only">
+          Filter GoFundMes
+        </label>
+        <input
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-1 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder:text-gray-600 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          onChange={handleChange}
+          placeholder="Filter by title"
+          type="text"
+          id="filter"
+        />
+      </div>
+      <div className="flex flex-wrap">
+        {filtered.map(
+          (gfm: {
+            title: string;
+            url: string;
+            imageurl: string;
+            progress: string;
+          }) => {
+            return (
+              <Card
+                title={gfm.title}
+                imageUrl={gfm.imageurl}
+                progress={gfm.progress}
+                url={gfm.url}
+                target=""
+                key={gfm.url}
+              />
+            );
+          },
+        )}
+      </div>
+    </>
   );
 }
